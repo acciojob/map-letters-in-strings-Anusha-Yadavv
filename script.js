26ead202bf0bd340dfec2387a98f0ba3c5678af9
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-19 16:21:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+function mapLetters(string) {
+	let obj={};
+	let n=string.length;
+	for(let i=0;i<n;i++){
+		let letter=string[i];
+		if(!obj[letter]){
+			obj[letter]=[i];
+		}else{
+			obj[letter].push(i);
+		}
+	}
+	return obj;
+	
+	
+	
+}
+
+const string = prompt("Enter string.");
+alert(string);
